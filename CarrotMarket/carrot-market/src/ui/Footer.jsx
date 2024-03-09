f6b6f74236b62c46673d670fe272d73bae42bc68
ljh37694 <ljh37694@naver.com>
2024-03-09 15:45:13 +0900

    메뉴 클릭하면 회색으로 변하는 기능 추가

--- CarrotMarket/carrot-market/src/ui/Footer.jsx
+++ CarrotMarket/carrot-market/src/ui/Footer.jsx
@@ -8,14 +8,21 @@ import {
     faComments,
     faUser,
 } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
 
 function Footer(props) {
+    let [selectMenu, setSelectMenu] = useState(0);
+
     const FooterContainer = styled.footer`
         color: #fff;
         position: absolute;
-        bottom: 0px;
+        bottom: -20px;
         width: 100%;
         text-align: center;
+
+        &:focus {
+            color: #6e6e6e;
+        }
     `;
 
     const Menu = styled.div`
@@ -27,41 +34,42 @@ function Footer(props) {
 
     const MenuTitle = styled.p`
         font-size: 14px;
+        margin-top: 5px;
     `;
+
+    const footerMenuData = [
+        { icon : faHouse, title : "홈" },
+        { icon : faBuilding, title : "동네생활" },
+        { icon : faLocationDot, title : "내 근처" },
+        { icon : faComments, title : "채팅" },
+        { icon : faUser, title : "나의 당근" },
+    ]
+
+    useEffect(() => {
+        const menuBtn = document.querySelectorAll(".menu-btn");
+
+        [...menuBtn].forEach((data) => {
+            data.style.color = "#fff";
+        });
+
+        menuBtn[selectMenu].style.color = "#6e6e6e";
+    }, [selectMenu]);
 
     return (
         <FooterContainer>
             <Row>
-                <Col>
-                    <Menu>
-                        <FontAwesomeIcon icon={faHouse} />
-                        <MenuTitle>홈</MenuTitle>
-                    </Menu>
-                </Col>
-                <Col>
-                    <Menu>
-                        <FontAwesomeIcon icon={faBuilding} />
-                        <MenuTitle>동네생활</MenuTitle>
-                    </Menu>
-                </Col>
-                <Col>
-                    <Menu>
-                        <FontAwesomeIcon icon={faLocationDot} />
-                        <MenuTitle>내 근처</MenuTitle>
-                    </Menu>
-                </Col>
-                <Col>
-                    <Menu>
-                        <FontAwesomeIcon icon={faComments} />
-                        <MenuTitle>채팅</MenuTitle>
-                    </Menu>
-                </Col>
-                <Col>
-                    <Menu>
-                        <FontAwesomeIcon icon={faUser} />
-                        <MenuTitle>나의 당근</MenuTitle>
-                    </Menu>
-                </Col>
+                { 
+                    footerMenuData.map((data, idx) => {
+                        return (
+                            <Col>
+                                <Menu className="menu-btn" data-id={idx} onClick={() => setSelectMenu(idx)}>
+                                    <FontAwesomeIcon icon={data.icon} />
+                                    <MenuTitle>{data.title}</MenuTitle>
+                                </Menu>
+                            </Col>
+                        );
+                    })
+                }
             </Row>
         </FooterContainer>
     );
