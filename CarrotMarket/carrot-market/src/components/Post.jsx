b2ea28a47f9a526203ca78b2b69a493c2167816c
ljh37694 <ljh37694@naver.com>
2024-03-11 22:44:18 +0900

    포스트에 채팅 및 하트 수 표시 기능 추가

--- CarrotMarket/carrot-market/src/components/Post.jsx
+++ CarrotMarket/carrot-market/src/components/Post.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { faComments, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Post(props) {
     let { posts } = props;
@@ -8,18 +10,18 @@ function Post(props) {
         color: #fff;
         box-sizing: border-box;
         border-bottom: 1px solid #6e6e6e;
-        margin-bottom: 20px;
+        padding: 15px 0px;
     `;
 
     const ContentsContainer = styled.div`
         display: flex;
         flex-direction: column;
         margin-left: 20px;
+        flex-grow: 1;
     `;
 
     const PostImage = styled.img`
         width: 8em;
-        height: 8em;  
     `;
 
     const PostTitle = styled.h3`
@@ -36,6 +38,27 @@ function Post(props) {
         font-weight: bold;
     `;
 
+    const PostFooter = styled.div`
+        display: flex;
+        justify-content: end;
+        text-align: end;
+    `;
+
+    const CountContainer = styled.div`
+        color: #fff;
+        display: flex;
+        align-items: center;
+        
+        & > * {
+            padding: 0px;
+            margin: 0px;
+        }
+
+        & > p {
+            margin: 0px 6px;
+        }
+    `;
+
     return (
         <PostContainer>
             <PostImage src="https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png">
@@ -44,6 +67,16 @@ function Post(props) {
                 <PostTitle>품절 임박! 닌텐도 스위치 팝니다</PostTitle>
                 <PostDetail>송정동 · 3시간 전</PostDetail>
                 <PostPrice>230,000원</PostPrice>
+                <PostFooter>
+                    <CountContainer>
+                        <FontAwesomeIcon icon={faComments}/>
+                        <p>1</p>
+                    </CountContainer>
+                    <CountContainer>
+                        <FontAwesomeIcon icon={faHeart}/>
+                        <p>5</p>
+                    </CountContainer>
+                </PostFooter>
             </ContentsContainer>
         </PostContainer>
     );
