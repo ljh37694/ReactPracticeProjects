2abe2ee869b0bdc369324014a831846f4125ecef
ljh37694 <ljh37694@naver.com>
2024-03-16 19:01:27 +0900

    post에 사용할 data 대충 만듦

---
+++ CarrotMarket/carrot-market/src/postData.js
@@ -0,0 +1,64 @@
+let postData = [
+    { 
+        title: "닌텐도 스위치 팝니다",
+        photo: ["https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png", ],
+        price: 230000,
+        content: "싸게 팝니다."
+    },
+    { 
+        title: "품절 임박! 닌텐도 스위치 팝니다",
+        photo: ["https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png", ],
+        price: 230000,
+        content: "싸게 팝니다."
+    },
+    { 
+        title: "품절 임박! 닌텐도 스위치 팝니다",
+        photo: ["https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png", ],
+        price: 230000,
+        content: "싸게 팝니다."
+    },
+    { 
+        title: "품절 임박! 닌텐도 스위치 팝니다",
+        photo: ["https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png", ],
+        price: 230000,
+        content: "싸게 팝니다."
+    },
+    { 
+        title: "품절 임박! 닌텐도 스위치 팝니다",
+        photo: ["https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png", ],
+        price: 230000,
+        content: "싸게 팝니다."
+    },
+    { 
+        title: "품절 임박! 닌텐도 스위치 팝니다",
+        photo: ["https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png", ],
+        price: 230000,
+        content: "싸게 팝니다."
+    },
+    { 
+        title: "품절 임박! 닌텐도 스위치 팝니다",
+        photo: ["https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png", ],
+        price: 230000,
+        content: "싸게 팝니다."
+    },
+    { 
+        title: "품절 임박! 닌텐도 스위치 팝니다",
+        photo: ["https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png", ],
+        price: 230000,
+        content: "싸게 팝니다."
+    },
+    { 
+        title: "품절 임박! 닌텐도 스위치 팝니다",
+        photo: ["https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png", ],
+        price: 230000,
+        content: "싸게 팝니다."
+    },
+    { 
+        title: "품절 임박! 닌텐도 스위치 팝니다",
+        photo: ["https://blog.kakaocdn.net/dn/tWx11/btqDag5h7y3/6aY75vJNfOPMyNicZo53c0/img.png", ],
+        price: 230000,
+        content: "싸게 팝니다."
+    },
+];
+
+export default postData;
