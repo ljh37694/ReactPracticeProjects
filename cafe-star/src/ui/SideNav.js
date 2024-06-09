c3abffbbc3f990b4b09a483b744ac12fc08b1187
i_junghoon <ljh37694@naver.com>
2024-06-09 15:17:54 +0900

    SideNavbar 만듦

--- cafe-star/src/ui/SideNav.js
+++ cafe-star/src/ui/SideNav.js
@@ -1,7 +1,26 @@
+import { faHouse, faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
 function SideNav(props) {
+  let [activeMenu, setActiveMenu] = useState(0);
+  const icons = [faHouse, faStar];
+
   return (
     <nav className="side-nav">
-      hi
+      <div className="brand-icon-container">
+        <p>Cesr</p>
+      </div>
+
+      <div>
+        {icons.map((icon, idx) => {
+          return (
+            <div className={`nav-menu-icon ${activeMenu === idx ? "nav-menu-active-icon" : ""}`} onClick={() => {setActiveMenu(idx)}} key={idx}>
+              <FontAwesomeIcon icon={icon} size="2x"/>
+            </div>
+          );
+        })}
+      </div>
     </nav>
   );
 }
