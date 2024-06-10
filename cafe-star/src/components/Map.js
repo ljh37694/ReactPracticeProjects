5c0c23cfa46530f76e72ac323e3942ef5130334e
i_junghoon <ljh37694@naver.com>
2024-06-10 14:30:56 +0900

    카카오맵 api 대충 해봄

---
+++ cafe-star/src/components/Map.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+const { kakao } = window;
+
+function Map(props) {
+  useEffect(() => {
+		const container = document.getElementById('map');
+    
+		const options = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
+
+		const map = new kakao.maps.Map(container, options);
+  }, []);
+
+  return (
+    <div id="map"></div>
+  );
+}
+
+export default Map;
--- cafe-star/src/pages/MainPage.js
+++ cafe-star/src/pages/MainPage.js
@@ -1,11 +1,13 @@
 import SideNav from "../ui/SideNav";
 import MainPanel from "../ui/MainPanel";
+import Map from "../components/Map";
 
 function MainPage(props) {
   return (
     <main id="main-page">
       <SideNav />
       <MainPanel />
+      <Map />
     </main>
   );
 }
