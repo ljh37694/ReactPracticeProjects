cb37402d5c20def23559954693aae90af41600ab
i_junghoon <ljh37694@naver.com>
2024-06-12 21:12:55 +0900

    검색창 디자인 수정

--- CafeStar/cafe-star/src/ui/MainPanel.js
+++ CafeStar/cafe-star/src/ui/MainPanel.js
@@ -1,35 +1,63 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faChevronRight, faMagnifyingGlass, faX } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronLeft,
+  faChevronRight,
+  faMagnifyingGlass,
+  faX,
+} from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 function MainPanel(props) {
   let [isClose, setIsClose] = useState(false);
+  let [activeX, setActiveX] = useState(false);
 
   return (
-    <div className={`main-panel ${ isClose === true ? "main-panel-close" : "" }`}>
-      <button className="main-panel-close-button" onClick={() => setIsClose(!isClose)}>
-        <FontAwesomeIcon icon={ isClose === true ? faChevronLeft : faChevronRight} />
+    <div className={`main-panel ${isClose === true ? "main-panel-close" : ""}`}>
+      <button
+        className="main-panel-close-button"
+        onClick={() => setIsClose(!isClose)}
+      >
+        <FontAwesomeIcon
+          icon={isClose === true ? faChevronLeft : faChevronRight}
+        />
       </button>
 
       <div className="search-input-container">
         <div className="search-icon">
-          <FontAwesomeIcon icon={ faMagnifyingGlass } />
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
         </div>
-        <form className="search-form" onSubmit={(e) => {
-          e.preventDefault();
+        <form
+          className="search-form"
+          onSubmit={(e) => {
+            e.preventDefault();
 
-          const text = document.getElementById('search-input').value;
+            const text = document.getElementById("search-input").value;
 
-          console.log(text);
-
-          fetch('http://localhost:5000/search?query=' + text)
-          .then((res) => res.json())
-          .then((data) => console.log(data));
-        }}>
-          <input id="search-input" />
+            fetch("http://localhost:5000/search?query=" + text)
+              .then((res) => res.json())
+              .then((data) => console.log(data));
+          }}
+        >
+          <input
+            id="search-input"
+            onInput={(e) => {
+              if (e.target.value === "") {
+                setActiveX(false);
+              } else {
+                setActiveX(true);
+              }
+            }}
+          />
         </form>
         <div className="x-button">
-          <FontAwesomeIcon icon={faX} />
+          {activeX === true ? (
+            <FontAwesomeIcon
+              icon={faX}
+              onClick={() => {
+                document.getElementById("search-input").value = "";
+              }}
+            />
+          ) : null}
         </div>
       </div>
     </div>
