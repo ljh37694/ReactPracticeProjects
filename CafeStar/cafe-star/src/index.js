f38237b67a34af918f15331a797a0b25fb90d462
i_junghoon <ljh37694@naver.com>
2024-06-13 12:52:50 +0900

    redux toolkit 설치

--- CafeStar/cafe-star/src/index.js
+++ CafeStar/cafe-star/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import MainPage from './pages/MainPage';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MainPage />
+    <Provider store={store}>
+      <MainPage />
+    </Provider>
   </React.StrictMode>
 );
 
