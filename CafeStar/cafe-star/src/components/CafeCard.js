a8a96df9f2314f9759d0029b9ced17fa7491ea7d
i_junghoon <ljh37694@naver.com>
2024-06-14 13:33:25 +0900

    별점 디자인만 구현

--- CafeStar/cafe-star/src/components/CafeCard.js
+++ CafeStar/cafe-star/src/components/CafeCard.js
@@ -1,8 +1,11 @@
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSelector } from "react-redux";
 
 function CafeCard(props) {
   const { data } = props;
   const kakaoMap = useSelector(state => state.kakaoMap.value);
+  const star = 5.0;
 
   return (
     <div className="cafe-card" onClick={() => {
@@ -10,6 +13,19 @@ function CafeCard(props) {
     }}>
       <a className="cafe-card-title" href={ data.place_url }>{ data.place_name }</a>
       <p className="cafe-card-address">{ data.address_name }</p>
+
+      <div className="cafe-card-rating-container">
+        <div className="cafe-card-star-container">
+          {
+            [...new Array(star)].map(() => {
+              return (
+                <FontAwesomeIcon icon={faStar} />
+              );
+            })
+          }
+        </div>
+        <p>{star.toFixed(1)}</p>
+      </div>
     </div>
   );
 }
