4b35478215d975f9764e099b8a5e26a1fb19bb46
i_junghoon <ljh37694@naver.com>
2024-06-29 11:05:32 +0900

    form을 이용해 아이디와 비밀먼호를 서버에 POST 하는 기능 구현

--- CafeStar/cafe-star/src/pages/LoginPage.jsx
+++ CafeStar/cafe-star/src/pages/LoginPage.jsx
@@ -5,9 +5,32 @@ function LoginPage(props) {
         <header>
           <h4 className="title login-title">Cafe Star</h4>
         </header>
-        <form className="login-form">
-          <input className="login-form-input" placeholder="아이디"/>
-          <input className="login-form-input" placeholder="비밀번호" />
+        <form className="login-form" name="loginForm" onSubmit={(e) => {
+          e.preventDefault();
+
+          const id = document.loginForm.id.value;
+          const pw = document.loginForm.pw.value;
+
+          if (id === '' || pw === '') {
+            alert("아이디 또는 비밀번호가 빈 칸입니다.");
+          } else {
+            console.log(id, pw);
+
+            fetch(process.env.REACT_APP_SERVER_URL + "/login", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                id,
+                pw,
+              })
+            })
+            .catch(e => console.log(e));
+          }
+        }}>
+          <input type="text" className="login-form-input" name="id" placeholder="아이디"/>
+          <input type="password" className="login-form-input" name="pw" placeholder="비밀번호" autoComplete="off" />
           <button type="submit" className="login-button">로그인</button>
         </form>
       </section>
